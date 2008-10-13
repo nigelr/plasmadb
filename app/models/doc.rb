e4fa0da25d90870a191e22cbe65d118e532a3021
Nigel Rausch <nigelrausch@nigel-rauschs-mac-pro-3.local>
2008-10-13 17:56:08 +1000

    Changed revisoin support to be more efficient

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,5 +1,5 @@
 class Doc < ActiveRecord::Base
-  has_many :stores , :conditions => 'stores.version = 1'
+  has_many :stores , :conditions => 'stores.version is null'
   has_many :all_stores,
     :class_name => "Store",
     :foreign_key => "doc_id"
@@ -13,9 +13,11 @@ class Doc < ActiveRecord::Base
   def self.store items
     if items.is_a? Hash
       doc = self.find_or_create_by_id(items[:_id])
-      doc.update_attribute(:rev, doc.rev.to_i+1)
       #      puts "doc.rev.to_i+1=#{doc.rev}"
       doc.store_items items
+      doc.update_attribute(:rev, doc.rev.to_i+1)
+      doc.retrieve
+
     end
   end
 
@@ -51,7 +53,6 @@ class Doc < ActiveRecord::Base
   def store_items items
     #    delete_old_items items
     update_items items
-    retrieve
   end
 
   #  def delete_old_items items
@@ -65,11 +66,11 @@ class Doc < ActiveRecord::Base
   #  end
 
   def update_items items
-    all_stores.update_all("version=version+1")
+    stores.update_all("version=#{rev}")
     for field_name, item in items
       unless field_name.to_s == "_id"
         field = Field.find_or_create_by_name(field_name.to_s)
-        stores.create(:field => field, :data => item, :version => 1)
+        stores.create(:field => field, :data => item, :version => nil)
         #        puts "rev=#{rev}"
         #        store.update_attribute(:rev, rev)
       end
